c3bfc41adf7d041fd483605b5f2c1865b9569ff6
Thordur Petur <thordurpetur@gmail.com>
2025-02-21 11:15:39 +0000

    stuff

---
+++ Loka_Verkefni/target/debug/deps/Loka_Verkefni-675482d567ee8afd.d
@@ -0,0 +1,5 @@
+C:\Users\thord\OneDrive\Desktop\Rust_Lokaverkefni\Loka_Verkefni\target\debug\deps\libLoka_Verkefni-675482d567ee8afd.rmeta: src/main.rs
+
+C:\Users\thord\OneDrive\Desktop\Rust_Lokaverkefni\Loka_Verkefni\target\debug\deps\Loka_Verkefni-675482d567ee8afd.d: src/main.rs
+
+src/main.rs:
---
+++ Loka_Verkefni/target/debug/deps/Loka_Verkefni-e2878301ddd0f161.d
@@ -0,0 +1,5 @@
+C:\Users\thord\OneDrive\Desktop\Rust_Lokaverkefni\Loka_Verkefni\target\debug\deps\libLoka_Verkefni-e2878301ddd0f161.rmeta: src/main.rs
+
+C:\Users\thord\OneDrive\Desktop\Rust_Lokaverkefni\Loka_Verkefni\target\debug\deps\Loka_Verkefni-e2878301ddd0f161.d: src/main.rs
+
+src/main.rs:
